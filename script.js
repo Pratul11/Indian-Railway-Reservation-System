b9547dfb6baaf5e243503c777fce4d1baada1726
Pratul11 <pratuljohari99@gmail.com>
2024-02-18 23:22:27 +0530

    final touch of the page

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function toggleMenu() {
-  var links = document.querySelectorAll(".menu-bar a");
+  var links = document.querySelectorAll(".menu-btn a");
   links.forEach(function (link) {
     link.style.display =
       link.style.display === "none" ? "inline-block" : "none";
   });
 }
+
+// Disable the previous dates
+
+$(function () {
+  var dtToday = new Date();
+
+  var month = dtToday.getMonth() + 1;
+  var day = dtToday.getDate();
+  var year = dtToday.getFullYear();
+  if (month < 10) month = "0" + month.toString();
+  if (day < 10) day = "0" + day.toString();
+  var maxDate = year + "-" + month + "-" + day;
+  $("#date").attr("min", maxDate);
+});
